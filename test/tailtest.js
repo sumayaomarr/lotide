f4241734d52943206e6732b397d6380e9ed0f5ca
sumaya <sumayaomarr@gmail.com>
2021-11-29 23:48:42 -0500

    refractored

---
+++ test/tailtest.js
@@ -0,0 +1,11 @@
+
+const assertEqual = require('../assertEqual');
+
+const tail = require('../tail');
+
+
+// TEST CODE
+assertEqual(tail("Lighthouse Labs", "Bootcamp"));
+assertEqual(1, 1);
+
+return tail;
